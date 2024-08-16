de096e6b7d950d561c90b9aa82b1d373162b7282
GABRIEL DE OLIVEIRA LONGAS <081200002@faculdade.cefsa.edu.br>
2024-08-16 20:43:51 -0300

    nenhum caracter está longe do nosso alcançe

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,39 +1,33 @@
-def encrypt(text, shift):
-    encrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            shift_amount = 65 if char.isupper() else 97
-            encrypted_text += chr((ord(char) - shift_amount + shift) % 26 + shift_amount)
-        else:
-            encrypted_text += char
-    return encrypted_text
+#def encrypt(text, shift):
+#    encrypted_text = ""
+#    for char in text:
+#        if char.isalpha():
+#            shift_amount = 65 if char.isupper() else 97
+#            encrypted_text += chr((ord(char) - shift_amount + shift) % 26 + shift_amount)
+#        else:
+#            encrypted_text += char
+#    return encrypted_text
 
-def decrypt(text, shift):
-    return encrypt(text, -shift)
+#def decrypt(text, shift):
+#    return encrypt(text, -shift)
+
 
 def caesar_encrypt(text: str, shift: int) -> str:
     result = []
 
-    for char in text:
-        if char.isalpha():
-            offset = 65 if char.isupper() else 97
-            shifted_char = chr((ord(char) - offset + shift) % 26 + offset)
-            result.append(shifted_char)
-        else:
-            result.append(char)
-
+    for char in text:      
+        shifted_char = chr((ord(char) + shift))
+        result.append(shifted_char)
     return ''.join(result)
-
 
 def caesar_decrypt(text: str, shift: int) -> str:
-    result = []
+    return caesar_encrypt(text, -shift)
 
-    for char in text:
-        if char.isalpha(): # check if is letter
-            offset = 65 if char.isupper() else 97
-            shifted_char = chr((ord(char) - offset - shift) % 26 + offset)
-            result.append(shifted_char)
-        else:
-            result.append(char)
-
-    return ''.join(result)
+#def caesar_decrypt(text: str, shift: int) -> str:
+#    result = []
+#
+#    for char in text:
+#        offset = 65 if char.isupper() else 97
+#        shifted_char = chr((ord(char) - offset - shift) % 26 + offset)
+#        result.append(shifted_char)
+#    return ''.join(result)
